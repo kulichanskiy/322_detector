15b3f841f6b1a94eab419d9609efdb3b453cc9f4
c1y256 <etaranov265@gmail.com>
2024-05-01 14:14:11 +0300

    Парсинг названий турнира + чуть чуть функий для парсинга команд и кэфов

--- parsers/1xbet/1x_parser.py
+++ parsers/1xbet/1x_parser.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-headers = {
+headers = {  #Что бы сайты не детектили
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
     "Cookie": "platform_type=desktop; auid=U5PNIGYv0fo0xkTZA0y5Ag==; is12h=0; lng=en; SLG_G_WPT_TO=ru; SESSION=d0c0d50fb11165223e1f381a32b94251; tzo=3; _glhf=1714427759; che_g=a2119ffd-701c-e4c8-5571-aabfaedf20ba; SLG_GWPT_Show_Hide_tmp=1; SLG_wptGlobTipTmp=1; ggru=167; application_locale=en; sh.session.id=c41c7aa6-2640-4836-9900-343dac3df917; che_i=1; cookies_agree_type=3; _gcl_au=1.1.923709018.1714410186; _ga_4MKFXHBFXH=GS1.1.1714410186.1.0.1714410186.0.0.0; _ga=GA1.1.610647392.1714410186; window_width=898"
 }
+
 r = requests.get("https://1xbet.com/en/live/esports")
 soup = bs(r.content, 'lxml')
 data = soup.find("div", class_="ui-dashboard dashboard betting-dashboard__app")
--- parsers/winline/winline_parser.py
+++ parsers/winline/winline_parser.py
@@ -4,39 +4,58 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup as bs
 
+
 def get_html_content(url):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')  # Запускаем Chrome в режиме headless (без отображения GUI)
+    chrome_options.add_argument(
+        "--headless"
+    )  # Запускаем Chrome в режиме headless (без отображения GUI)
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
     # Добавляем задержку до 10 секунд, чтобы дождаться загрузки страницы
     wait = WebDriverWait(driver, 7)
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "block-sport__champ-list")))
+    wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "block-sport__champ-list"))
+    )
     html_content = driver.page_source
     driver.quit()
     return html_content
+
+
+def get_tournament(soup):
+    tournament_data = soup.find_all("div", class_="block-sport__champ-item ng-star-inserted")
+    with open("1.txt", "a", encoding="utf-8") as f:
+        for i in tournament_data:
+            tournament_name = i.find('span', class_='block-tournament-header__title')
+            f.write(tournament_name.text.strip() + '\n')
+
+
+
+
+
+# team_data = i.find_all('div', class_='name ng-star-inserted')
+#             for j in team_data:
+#                 team = j
+                
+#                 #team_coef_2 = tournament_data.find('div', class_='coefficient-button coefficient-button_fill coefficient-button_align_space-b coefficient-button_generic2 coefficient-button_align_center ng-star-inserted coefficient-button_down')
+#                 f.write(j.text.strip() + '\n')
+#             f.write('\n')
+            
 
 # soup = bs(html_content, 'lxml')
 
 urls = [
     "https://winline.ru/stavki/sport/kibersport/counter-strike",
-    "https://winline.ru/stavki/sport/kibersport/dota_2",
-    "https://winline.ru/stavki/sport/kibersport/valorant",
-    "https://winline.ru/stavki/sport/kibersport/league_of_legends",
-    "https://winline.ru/stavki/sport/kibersport/rainbow_six"
+    # "https://winline.ru/stavki/sport/kibersport/dota_2",
+    # "https://winline.ru/stavki/sport/kibersport/valorant",
+    # "https://winline.ru/stavki/sport/kibersport/league_of_legends",
+    # "https://winline.ru/stavki/sport/kibersport/rainbow_six",
 ]
 
 for url in urls:
     html_content = get_html_content(url)
-    soup = bs(html_content, 'lxml')
+    soup = bs(html_content, "lxml")
     filename = url.replace("https://winline.ru/stavki/sport/kibersport/", "") + ".html"
     with open(filename, "w", encoding="utf-8") as f:
-     f.write(soup.prettify())
-
-
-
-
-
-    
-
-
+        f.write(soup.prettify())
+    get_tournament(soup)
